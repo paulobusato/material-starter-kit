02d08b0d9b8a52e7e98feb95f440ea35891263d2
Paulo Busato <paulo.busato2@gmail.com>
2019-09-20 12:01:21 -0300

    alterado background, alterado movimento entre tabs

--- app.js
+++ app.js
@@ -7,3 +7,14 @@ const tab = new MDCTab(document.querySelector('.mdc-tab'));
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 const tabIndicator = new MDCTabIndicator(document.querySelector('.mdc-tab-indicator'));
 const tabScroller = new MDCTabScroller(document.querySelector('.mdc-tab-scroller'));
+
+
+tabBar.listen('MDCTabBar:activated', activatedEvent => {
+  document.querySelectorAll('.tab-content').forEach((element, index) => {
+    if (index === activatedEvent.detail.index) {
+      element.classList.remove('display-hidden');
+    } else {
+      element.classList.add('display-hidden');
+    }
+  });
+});
