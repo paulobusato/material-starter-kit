fb752beea0dff5034f10aff72243a3dfac4d8685
Paulo Busato <paulo.busato2@gmail.com>
2019-09-20 20:40:26 -0300

    iniciado criacaco da tabela de pedidos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import {MDCTab} from '@material/tab';
 import {MDCTabBar} from '@material/tab-bar';
 import { MDCTabIndicator } from "@material/tab-indicator";
 import { MDCTabScroller } from "@material/tab-scroller";
+import { MDCDataTable } from "@material/data-table";
 
 const tab = new MDCTab(document.querySelector('.mdc-tab'));
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
