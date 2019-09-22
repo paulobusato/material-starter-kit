3cbce39f012f37d7bb94ce6d53c8ec4689f8061b
Paulo Busato <paulo.busato2@gmail.com>
2019-09-22 18:41:14 -0300

    update

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-import {MDCTab} from '@material/tab';
-import {MDCTabBar} from '@material/tab-bar';
-import { MDCTabIndicator } from "@material/tab-indicator";
-import { MDCTabScroller } from "@material/tab-scroller";
-import { MDCDataTable } from "@material/data-table";
+import { MDCTextField } from "@material/textfield";
+import {MDCList} from "@material/list";
+import { MDCRipple } from "@material/ripple";
 
-const tab = new MDCTab(document.querySelector('.mdc-tab'));
-const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
-const tabIndicator = new MDCTabIndicator(document.querySelector('.mdc-tab-indicator'));
-const tabScroller = new MDCTabScroller(document.querySelector('.mdc-tab-scroller'));
+const list = MDCList.attachTo(document.querySelector('.mdc-list'));
+list.wrapFocus = true;
 
-
-tabBar.listen('MDCTabBar:activated', activatedEvent => {
-  document.querySelectorAll('.tab-content').forEach((element, index) => {
-    if (index === activatedEvent.detail.index) {
-      element.classList.remove('display-hidden');
-    } else {
-      element.classList.add('display-hidden');
-    }
-  });
+const selector = '.mdc-button, .mdc-icon-button .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelector(selector), function (el) {
+  return new MDCRipple(el);
 });
